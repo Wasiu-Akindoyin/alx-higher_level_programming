4e99b57526e8e79bb5e0043112f1c8f2f012e7e8
Wasiu-Akindoyin <wasiuwazzy@gmail.com>
2023-03-15 15:07:53 +0100

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(elem => console.log(elem));
+const myVar1 = "C is fun";
+const myVar2 = "Python is cool";
+const myVar3 = "JavaScript is amazing";
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
